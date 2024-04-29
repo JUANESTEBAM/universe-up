cacbd8024fd591ad885348da723be2d58bd41f45
LuisaBuso <luisafbs3@gmail.com>
2024-04-29 14:19:41 -0500

    first commit

---
+++ services.py
@@ -0,0 +1,327 @@
+import requests
+import sett
+import json
+import time
+import openai
+
+
+openai.api_key = 'insertar'
+
+
+def obtener_Mensaje_whatsapp(message):
+    if 'type' not in message :
+        text = 'mensaje no reconocido'
+        return text
+
+    typeMessage = message['type']
+    if typeMessage == 'text':
+        text = message['text']['body']
+    elif typeMessage == 'button':
+        text = message['button']['text']
+    elif typeMessage == 'interactive' and message['interactive']['type'] == 'list_reply':
+        text = message['interactive']['list_reply']['title']
+    elif typeMessage == 'interactive' and message['interactive']['type'] == 'button_reply':
+        text = message['interactive']['button_reply']['title']
+    else:
+        text = 'mensaje no procesado'
+    
+    
+    return text
+
+def enviar_Mensaje_whatsapp(data):
+    try:
+        whatsapp_token = sett.whatsapp_token
+        whatsapp_url = sett.whatsapp_url
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': 'Bearer ' + whatsapp_token}
+        print("se envia ", data)
+        response = requests.post(whatsapp_url, 
+                                 headers=headers, 
+                                 data=data)
+        
+        if response.status_code == 200:
+            return 'mensaje enviado', 200
+        else:
+            return 'error al enviar mensaje', response.status_code
+    except Exception as e:
+        return e,403
+    
+def text_Message(number,text):
+    data = json.dumps(
+            {
+                "messaging_product": "whatsapp",    
+                "recipient_type": "individual",
+                "to": number,
+                "type": "text",
+                "text": {
+                    "body": text
+                }
+            }
+    )
+    return data
+
+def buttonReply_Message(number, options, body, footer, sedd,messageId):
+    buttons = []
+    for i, option in enumerate(options):
+        buttons.append(
+            {
+                "type": "reply",
+                "reply": {
+                    "id": sedd + "_btn_" + str(i+1),
+                    "title": option
+                }
+            }
+        )
+
+    data = json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "interactive",
+            "interactive": {
+                "type": "button",
+                "body": {
+                    "text": body
+                },
+                "footer": {
+                    "text": footer
+                },
+                "action": {
+                    "buttons": buttons
+                }
+            }
+        }
+    )
+    return data
+
+def listReply_Message(number, options, body, footer, sedd,messageId):
+    rows = []
+    for i, option in enumerate(options):
+        rows.append(
+            {
+                "id": sedd + "_row_" + str(i+1),
+                "title": option,
+                "description": ""
+            }
+        )
+
+    data = json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "interactive",
+            "interactive": {
+                "type": "list",
+                "body": {
+                    "text": body
+                },
+                "footer": {
+                    "text": footer
+                },
+                "action": {
+                    "button": "Ver Opciones",
+                    "sections": [
+                        {
+                            "title": "Secciones",
+                            "rows": rows
+                        }
+                    ]
+                }
+            }
+        }
+    )
+    return data
+
+def document_Message(number, url, caption, filename):
+    data = json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "document",
+            "document": {
+                "link": url,
+                "caption": caption,
+                "filename": filename
+            }
+        }
+    )
+    return data
+
+def sticker_Message(number, sticker_id):
+    data = json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "sticker",
+            "sticker": {
+                "id": sticker_id
+            }
+        }
+    )
+    return data
+
+def get_media_id(media_name , media_type):
+    media_id = ""
+    if media_type == "sticker":
+        media_id = sett.stickers.get(media_name, None)
+    #elif media_type == "image":
+    #    media_id = sett.images.get(media_name, None)
+    #elif media_type == "video":
+    #    media_id = sett.videos.get(media_name, None)
+    #elif media_type == "audio":
+    #    media_id = sett.audio.get(media_name, None)
+    return media_id
+
+def replyReaction_Message(number, messageId, emoji):
+    data = json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "reaction",
+            "reaction": {
+                "message_id": messageId,
+                "emoji": emoji
+            }
+        }
+    )
+    return data
+
+def replyText_Message(number, messageId, text):
+    data = json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "context": { "message_id": messageId },
+            "type": "text",
+            "text": {
+                "body": text
+            }
+        }
+    )
+    return data
+
+def markRead_Message(messageId):
+    data = json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "status": "read",
+            "message_id":  messageId
+        }
+    )
+    return data
+  
+usuarios = {}
+respuesta_usuarios= {}
+
+
+  
+def administrar_chatbot(text,number, messageId, name):
+    text = text.lower() #mensaje que envio el usuario
+    list = []
+    print("mensaje del usuario: ",text)
+
+    markRead = markRead_Message(messageId)
+    list.append(markRead)
+    time.sleep(2)
+
+    if "hola" in text:
+        body = "¡Hola! 👋 Bienvenido a Rizos Felices. ¿Cómo podemos ayudarte hoy?"
+        footer = "Rizos Felices"
+        options = ["✅ diagnostico", "📅 agendar cita"]
+
+        replyButtonData = buttonReply_Message(number, options, body, footer, "sed1",messageId)
+        replyReaction = replyReaction_Message(number, messageId, "🫡")
+        list.append(replyReaction)
+        list.append(replyButtonData)
+        
+        
+        
+        
+    elif "diagnostico" in text:
+        body = "Responde las siguientes preguntas para obtener tu diagnostico. ¿EL CABELLO TIENE CAPACIDAD DE FORMAR FACILMENTE EL RIZO ?"
+        footer = "Plasticidad"
+        options = ["Si", "No"]
+
+        replyButtonData = buttonReply_Message(number, options, body, footer, "sed2",messageId)
+        sticker = sticker_Message(number, get_media_id("perro_traje", "sticker"))
+
+        list.append(replyButtonData)
+        
+      
+        
+    elif  "si" in text:  
+         body = "EL CABELLO MOJA FÁCILMENTE?"
+         footer = "Permeabilidad"
+         options = ["✅ Sí", "⛔ No"]
+
+         replyButtonData = buttonReply_Message(number, options, body, footer, "sed3", messageId)
+         list.append(replyButtonData) 
+            
+    elif "no" in text: 
+            # Hacer la siguiente pregunta sobre densidad
+            body = "¿Cuál es la cantidad de cabello?"
+            footer = "Densidad"
+            options = ["poco", "Mucho"]
+            
+            replyButtonData = buttonReply_Message(number, options, body, footer, "sed4", messageId)
+            list.append(replyButtonData)  
+            
+        
+        
+        
+    elif "poco" in text: 
+            # Hacer la siguiente pregunta sobre densidad
+            body = "¿Cuál es la cantidad de cabellos?"
+            footer = "Densidad"
+            options = ["poco", "Mucho"]
+            
+            replyButtonData = buttonReply_Message(number, options, body, footer, "sed4", messageId)
+            list.append(replyButtonData)  
+            
+    elif "mucho" in text: 
+            # Hacer la siguiente pregunta sobre densidad
+            body = "¿Cuál es la cantidad de cabello2?"
+            footer = "Densidad"
+            options = ["si", "no"]
+            
+            replyButtonData = buttonReply_Message(number, options, body, footer, "sed4", messageId)
+            list.append(replyButtonData)  
+            
+    elif "si" in text: 
+            # Hacer la siguiente pregunta sobre densidad
+            body = "¿Cuál es la cantidad de cabello5?"
+            footer = "Densidad"
+            options = ["poco", "Mucho"]
+            
+            replyButtonData = buttonReply_Message(number, options, body, footer, "sed4", messageId)
+            list.append(replyButtonData)         
+        
+   
+    # También podrías enviar una lista de productos recomendados, etc.
+    else:
+        data = text_Message(number, "Lo siento, no entendí lo que dijiste. ¿Quieres que te ayude con alguna de estas opciones?")
+        list.append(data)
+
+    for item in list:
+        enviar_Mensaje_whatsapp(item)
+
+
+
+
+#indicador añadido para envio mensajes
+def replace_start(s):
+    number = s[3:]
+    if s.startswith("57"):
+        return "573" + number
+    elif s.startswith("549"):
+        return "54" + number
+    else:
+        return s
+        
+
---
+++ sett.py
@@ -0,0 +1,54 @@
+token = 'test'
+
+whatsapp_token = 'insertar'
+
+whatsapp_url = ' https://graph.facebook.com/v18.0/110779212014176/messages'
+
+
+openai_key = 'insertar'
+
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
+
+empresa_info = {
+    "servicios": {
+        "Corte": "Descripción del servicio 1",
+        "servicio_completo": "Descripción del servicio 2",
+        "servicio_completo_afro": "Descripción del servicio 3",
+        "transición": "Descripción del servicio 4",
+        "tratamientos": "Descripción del servicio 5",
+        "colorimetria": "Descripción del servicio 6",
+        "barberia": "Descripción del servicio 7",
+
+        # Agrega más servicios según sea necesario
+    },
+    "productos": {
+        "producto_1": {
+            "nombre": "Nombre del producto 1",
+            "descripcion": "Descripción del producto 1",
+            "precio": 100,  # Precio en la moneda correspondiente
+        },
+        "producto_2": {
+            "nombre": "Nombre del producto 2",
+            "descripcion": "Descripción del producto 2",
+            "precio": 150,
+        },
+        # Agrega más productos según sea necesario
+    }
+}
